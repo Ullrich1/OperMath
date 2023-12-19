90b69c2d5a2352f337ab864237992731a3c6919f
unknown <Ana Alice R do Nascimento>
2023-12-19 14:32:19 -0300

    Initial commit

---
+++ src/Contas.java
@@ -0,0 +1,13 @@
+public class Contas {
+    public static void mostrarCalculo(OperacaoMatematica operacao, double x, double y) {
+        System.out.println("O resultado é:" + operacao.calcular(x, y));
+    }
+
+
+    public static void main(String[] args) {
+        Contas.mostrarCalculo(new Soma(), 5,5);
+        Contas.mostrarCalculo(new Subtracao(), 5,5);
+        Contas.mostrarCalculo(new Multiplica(), 5,5);
+    }
+
+}
---
+++ src/Multiplica.java
@@ -0,0 +1,5 @@
+public class Multiplica extends OperacaoMatematica{
+    public double calcular(double x, double y){
+        return x*y;
+    }
+}
